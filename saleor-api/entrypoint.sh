03612b91e4cfb9bf5ef2baf1d7d4b87f29d335cb
argearhart <117050870+argearhart@users.noreply.github.com>
2025-02-11 15:24:38 -0500

    Refactor Dockerfile and entrypoint script; add read-only user creation script for database

--- saleor-api/entrypoint.sh
+++ saleor-api/entrypoint.sh
@@ -1,19 +1,11 @@
-#!/bin/bash
+##!/bin/bash
 set -e
 
-# Wait for database to be ready
-until PGPASSWORD=$POSTGRES_PASSWORD psql -h "db" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c '\q'; do
-  >&2 echo "Postgres is unavailable - sleeping"
-  sleep 1
-done
+echo "Applying database migrations..."
+python manage.py migrate
 
->&2 echo "Postgres is up - executing command"
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
 
-# Run migrations
-python3 manage.py migrate --settings=saleor.settings
-
-# Collect static files
-python3 manage.py collectstatic --no-input --settings=saleor.settings
-
-# Execute the main container command
+echo "Starting application..."
 exec "$@"
